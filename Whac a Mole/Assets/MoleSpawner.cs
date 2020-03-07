c040e3bdb74ba4adac0ea67efab75cf0dad01815
Clara <clara.daigmorte@gmail.com>
2020-03-07 13:49:22 +0100

    Create Unity project + game

---
+++ Whac a Mole/Assets/MoleSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoleSpawner : MonoBehaviour
+{
+    public GameObject molePrefab;
+    public Transform[] spawnPoints;
+    public float gameTime;
+    public Text gameText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+        if(gameTime < 1)
+        {
+            gameTime = 0;
+        }
+        gameText.text = gameTime.ToString();
+    }
+
+    // Spawn randomly the mole on a point of spawnPoints
+    public void Spawn()
+    {
+        GameObject mole = Instantiate(molePrefab) as GameObject;
+        mole.transform.position = spawnPoints[randomSpawn()].transform.position;
+    }
+
+    // Random à remplacer par les proba du prof
+    int randomSpawn()
+    {
+        return Random.Range(0, spawnPoints.Length);
+    }
+}
